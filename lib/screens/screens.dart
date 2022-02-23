4ca5ff2ed0bc7395e32bed599d14cc5692aad6fc
Ivan Rodriguez E <ivanrenescorcia@gmail.com>
2022-02-22 22:07:59 -0500

    initial commit

---
+++ lib/screens/screens.dart
@@ -0,0 +1,10 @@
+
+export 'package:panic_button_app/screens/check_auth_screen.dart';
+export 'package:panic_button_app/screens/home_screen.dart';
+export 'package:panic_button_app/screens/loading_screen.dart';
+export 'package:panic_button_app/screens/login_screen.dart';
+export 'package:panic_button_app/screens/product_screen.dart';
+export 'package:panic_button_app/screens/register_screen.dart';
+
+
+
